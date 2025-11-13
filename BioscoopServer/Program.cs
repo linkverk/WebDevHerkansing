f9c8b013b01e2cfd7ddf1f1c77998cb6223cfd53
AlperenBaytimur <alperen.baytimur@gmail.com>
2025-11-13 14:05:59 +0100

    Created the post service for reviews

--- BioscoopServer/Program.cs
+++ BioscoopServer/Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<CinemaContext>(options =>
     options.UseSqlite("Data Source=cinema.db"));
 
 builder.Services.AddScoped<DBFilmService>();
+builder.Services.AddScoped<ReviewServices>();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
