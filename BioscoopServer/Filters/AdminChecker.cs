ba3b12a7453d79561b401caa77fa7f3fe9311301
lesley-1303 <1075070@hr.nl>
2026-01-05 18:02:16 +0100

    laatste change

--- BioscoopServer/Filters/AdminChecker.cs
+++ BioscoopServer/Filters/AdminChecker.cs
@@ -7,12 +7,15 @@ public class AdminCheck : Attribute, IAsyncActionFilter
     public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
         var user = context.HttpContext.User;
-
         var role = user.FindFirst(ClaimTypes.Role)?.Value;
 
         if (role != "Admin")
         {
-            context.Result = new ForbidResult();
+            context.Result = new ContentResult
+            {
+                StatusCode = 403,
+                Content = "Access denied: You must be an admin to access this resource."
+            };
             return;
         }
 
